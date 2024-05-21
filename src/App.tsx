f73d8b5f0594830299b6aac941a653120d9d088b
Fashakin Olashubomi <subomifasakin@outlook.com>
2024-05-21 23:24:32 +0100

    sets users highscore in local storage, check if user is capable of playing, added a toat for highscore updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,84 @@
+import { useEffect, useState } from "react";
 import { LandingPage } from "./Components/LandingPage";
 import { TextDisplay } from "./Components/TextDisplay";
 
 import { StoreType, gameStore } from "./store";
+import { Button } from "./Components/Button";
+import { VscDebugStart } from "react-icons/vsc";
+
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   const { isPlaying } = gameStore<StoreType>(function (state) {
     return state;
   });
 
+  //get the width of the device to see if they are capable of playing the game
+  const [canPlay, setCanPay] = useState(function () {
+    return window.innerWidth >= 1024;
+  });
+
+  //if the users screen isnt big enough but they have an external keyboard
+  const [wantsToPlay, setWantsToPlay] = useState(true);
+
+  function userWantsToPlay() {
+    setCanPay(true);
+    setWantsToPlay(true);
+  }
+
+  //checks if the user resized their screen to one thats compatible or not
+  useEffect(function () {
+    function eventFn() {
+      const canPlay = window.innerWidth >= 1024;
+      setCanPay(canPlay);
+    }
+
+    window.addEventListener("resize", eventFn);
+
+    return () => window.removeEventListener("resize", eventFn);
+  }, []);
+
   return (
-    <div className="bg-black">
-      {!isPlaying ? <LandingPage /> : <TextDisplay />}
+    <div className="h-full w-full bg-black">
+      <Toaster
+        toastOptions={{
+          success: {
+            duration: 5 * 1000,
+            style: {
+              background: "#abf7b1",
+            },
+          },
+          error: {
+            duration: 1000 * 5,
+            style: {
+              background: "#ff6865",
+            },
+          },
+        }}
+      />
+
+      <div className={`${canPlay && wantsToPlay ? "block" : "hidden"}`}>
+        {!isPlaying ? <LandingPage /> : <TextDisplay />}
+      </div>
+
+      <div
+        className={`flex h-dvh flex-col gap-y-4 px-2 text-center ${!canPlay || !wantsToPlay ? "block" : "hidden"}  w-full items-center justify-center text-white lg:hidden`}
+      >
+        <p>It's like you are on a mobile device.</p>
+
+        <p className="text-xl font-bold text-yellow-300">
+          This test is catered to devices with a dedicated keyboard!
+        </p>
+
+        <p>However, If you have an external keyboard, Go Ahead! </p>
+
+        <Button
+          title="Play"
+          onClickFn={userWantsToPlay}
+          label={<VscDebugStart className="text-2xl" />}
+          rounded={true}
+        />
+      </div>
     </div>
   );
 }
--- src/Components/TextDisplay.tsx
+++ src/Components/TextDisplay.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useReducer } from "react";
+import { memo, useCallback, useEffect, useReducer, useState } from "react";
 
 import { StoreType, TimeStoreType, gameStore, timeStore } from "../store";
 
@@ -11,6 +11,7 @@ import { VscDebugRestart } from "react-icons/vsc";
 
 import { Button } from "./Button";
 import { Timer } from "./Timer";
+import toast from "react-hot-toast";
 
 type StateDetails = {
   clicked: number;
@@ -182,12 +183,16 @@ const Details = memo(function Details({
   });
 
   return (
-    <div className="flex cursor-default gap-x-3 rounded-lg border-2 border-stone-300 p-3 tracking-wider text-stone-300  ">
-      <p title="Difficulty" className="flex items-center gap-x-1">
-        <SlSettings className="text-lg" />: {difficulty}
-      </p>
+    <div className="flex cursor-default flex-col items-center gap-y-1 rounded-lg border-2 border-stone-300 p-3 tracking-wider text-stone-300  ">
+      <span className="text-xs text-stone-500">Game Details</span>
 
-      <Timer hasFinished={hasFinished} />
+      <div className="flex cursor-default  gap-x-3 ">
+        <p title="Difficulty" className="flex items-center gap-x-1">
+          <SlSettings className="text-lg" />: {difficulty}
+        </p>
+
+        <Timer hasFinished={hasFinished} />
+      </div>
     </div>
   );
 });
@@ -197,46 +202,158 @@ function Results({ points, totalChars, hasFinished }: ResultDetails) {
     return state;
   });
 
+  const { difficulty } = gameStore<StoreType>(function (state) {
+    return state;
+  });
+
+  //isHovering over results
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  //get the highScore for the difficulty from localStorage
+  const [highScore, setHighScore] = useState(function (): string {
+    const score = localStorage.getItem(difficulty);
+
+    //if there is a highscore return it, if not return an empty object
+    return score || JSON.stringify({ accuracy: 0, speed: 0 });
+  });
+
+  const jsonHighScore = JSON.parse(highScore);
+
+  ///if there is a highscore
+  const accuracyHighScore = jsonHighScore.accuracy;
+  const speedHighScore = jsonHighScore.speed;
+
   //all the points accumulated by the user
   const totalPoints = points.reduce((acc, curr) => {
     return acc + curr;
   }, 0);
 
-  //the users typing accuracy
+  //the users typing accuracy && speed
   const accuracy = (totalPoints / totalChars) * 100;
+  const speed = totalPoints / timeUsed;
 
-  //speed is the amount of correct words typed over time taken
-  const userSpeed = totalPoints / timeUsed;
-
-  //minimum speed required to completely type
+  //minimum speed & accuracy required to pass
+  const minAccuracy = 50;
   const minSpeed = totalChars / defaultTime;
 
-  return (
-    <div className="flex cursor-default  gap-x-3 rounded-lg border-2 border-stone-300 p-3  tracking-wider text-stone-300 ">
-      <p
-        title="Accuracy"
-        className={`flex items-center gap-x-1 ${hasFinished && accuracy > 50 ? "text-green-500" : ""} ${hasFinished && accuracy < 50 ? "text-red-500" : ""}`}
-      >
-        <SlTarget className="text-xl" title="accuracy" />: {accuracy.toFixed(2)}
-        %
-        {hasFinished ? (
-          <span className="items-center text-xs"> (min. {50}%)</span>
-        ) : null}
-      </p>
+  //what results are beign shown
+  const speedShown = isHovering ? speedHighScore : speed;
+  const accuracyShown = isHovering ? accuracyHighScore : accuracy;
 
-      <p
-        title="Speed"
-        className={`flex items-center gap-x-1 ${hasFinished && userSpeed > minSpeed ? "text-green-500" : ""} ${hasFinished && userSpeed < minSpeed ? "text-red-500" : ""}`}
-      >
-        <SlSpeedometer className="text-xl" />: {userSpeed.toFixed(2)}
-        <span className="text-sm">chars/s</span>
-        {hasFinished ? (
-          <span className="items-center text-xs">
-            {" "}
-            (min. {minSpeed}chars/s)
-          </span>
-        ) : null}
-      </p>
+  function hanldeMouseEnter() {
+    setIsHovering(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovering(false);
+  }
+
+  useEffect(
+    function () {
+      if (
+        hasFinished &&
+        accuracy > accuracyHighScore &&
+        speed <= speedHighScore
+      ) {
+        const updatedScores = JSON.stringify({
+          accuracy: accuracy.toFixed(2),
+          speed: speedHighScore,
+        });
+
+        localStorage.setItem(difficulty, updatedScores);
+
+        setHighScore(updatedScores);
+
+        //alert user
+        toast.success("New Accuracy HighScore");
+      }
+
+      if (
+        hasFinished &&
+        speed > speedHighScore &&
+        accuracy <= accuracyHighScore
+      ) {
+        const updatedScores = JSON.stringify({
+          accuracy: accuracyHighScore,
+          speed: speed.toFixed(2),
+        });
+
+        //set the new speed
+        localStorage.setItem(difficulty, updatedScores);
+
+        setHighScore(updatedScores);
+
+        //alert user
+        toast.success("New Speed HighScore");
+      }
+
+      if (
+        hasFinished &&
+        accuracy > accuracyHighScore &&
+        speed > speedHighScore
+      ) {
+        const updatedScores = JSON.stringify({
+          accuracy: accuracy,
+          speed: speed,
+        });
+
+        localStorage.setItem(difficulty, updatedScores);
+
+        setHighScore(updatedScores);
+
+        toast.success("New HighScore");
+      }
+    },
+    [
+      accuracy,
+      accuracyHighScore,
+      speed,
+      speedHighScore,
+      difficulty,
+      hasFinished,
+    ],
+  );
+
+  return (
+    <div
+      title={`High Score for ${difficulty}`}
+      onMouseEnter={hanldeMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="flex cursor-default flex-col items-center  gap-y-1 rounded-lg border-2 border-stone-300 p-3  tracking-wider text-stone-300 "
+    >
+      {isHovering ? (
+        <span className="text-xs text-stone-500">High Scores</span>
+      ) : (
+        <span className="text-xs text-stone-500">
+          Hover to View High Scores
+        </span>
+      )}
+
+      <div className="flex cursor-default  gap-x-3 ">
+        <p
+          title="Accuracy"
+          className={`results ${isHovering ? "text-white" : ""} ${!isHovering && hasFinished && accuracy > 50 ? "text-green-500" : ""} ${!isHovering && hasFinished && accuracy < 50 ? "text-red-500" : ""}`}
+        >
+          <SlTarget className="text-xl" title="accuracy" />:{" "}
+          {accuracyShown.toFixed(2)}%
+          {hasFinished ? (
+            <span className="items-center text-xs"> (min. {minAccuracy}%)</span>
+          ) : null}
+        </p>
+
+        <p
+          title="Speed"
+          className={`results ${isHovering ? "text-white" : ""} ${!isHovering && hasFinished && speed > minSpeed ? "text-green-500" : ""} ${!isHovering && hasFinished && speed < minSpeed ? "text-red-500" : ""}`}
+        >
+          <SlSpeedometer className="text-xl" />: {speedShown.toFixed(2)}
+          <span className="text-sm">chars/s</span>
+          {hasFinished ? (
+            <span className="items-center text-xs">
+              {" "}
+              (min. {minSpeed}chars/s)
+            </span>
+          ) : null}
+        </p>
+      </div>
     </div>
   );
 }
